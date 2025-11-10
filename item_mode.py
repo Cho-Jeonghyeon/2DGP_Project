e03cee3fa571cb174989e6f27c58f13d9967e1d3
Cho-Jeonghyeon <titanfall120@naver.com>
2025-11-10 09:33:55 +0900

    기본 구조

---
+++ item_mode.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import game_framework
+
+def init():
+    pass
+
+def finish():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
+
+def pause():
+    pass
+def resume():
+    pass
---
+++ stage_mode.py
@@ -0,0 +1,23 @@
+from pico2d import *
+import game_framework
+import game_mode
+
+def init():
+    pass
+
+def finish():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
+
+def pause():
+    pass
+def resume():
+    pass
