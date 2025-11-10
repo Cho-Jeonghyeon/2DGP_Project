aa66c6bf70ec56d91aed59078c621f7306bcf2c4
Cho-Jeonghyeon <titanfall120@naver.com>
2025-11-11 06:28:02 +0900

    drill 객체 생성

---
+++ drill.py
@@ -0,0 +1,24 @@
+from pico2d import *
+
+class Drill:
+    def __init__(self):
+        self.image = load_image('drill2.png')
+        self.frame = 0
+        self.level = 0
+        self.x, self.y = 400, 300
+
+    def update(self):
+        # 프레임 넘기기
+        self.frame = (self.frame + 1) % 6
+
+    def draw(self):
+        frame_width = 40
+        frame_height = 40
+
+        # clip_draw(x, y, w, h, draw_x, draw_y)
+        self.image.clip_draw(
+            self.frame * frame_width,                 # 현재 프레임의 가로 좌표
+            (5 - self.level) * frame_height,          # 세로 좌표 (pico2d는 아래가 0이므로 반대로 계산)
+            frame_width, frame_height,                # 한 프레임 크기
+            self.x, self.y                            # 그릴 위치
+        )
