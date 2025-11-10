f3ceedc1f9ef887d6be5b508c453c6a7e8ac5abd
Cho-Jeonghyeon <titanfall120@naver.com>
2025-11-10 13:01:27 +0900

    spaceship 객체 틀 구현

---
+++ spaceship.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+class Spaceship:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_events(self, event):
+        pass
+
+    def get_bb(self):
+        pass
+
