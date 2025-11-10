df71101e606beb86c0a1a2f07ce7f6c9a0654780
Cho-Jeonghyeon <titanfall120@naver.com>
2025-11-11 06:28:48 +0900

    방향키 함수 + 에니메이션

--- spaceship.py
+++ spaceship.py
@@ -1,19 +1,45 @@
 from pico2d import *
 from state_machine import StateMachine
 
+def right_down(event):
+    return event[0] == SDL_KEYDOWN and event[0] == SDLK_RIGHT
+
+def right_up(event):
+    return event[0] == SDL_KEYUP and event[0] == SDLK_RIGHT
+
+def left_up(event):
+    return event[0] == SDL_KEYUP and event[0] == SDLK_LEFT
+
+def left_down(event):
+    return event[0] == SDL_KEYDOWN and event[0] == SDLK_LEFT
+
+def down_down(event):
+    return event[0] == SDL_KEYDOWN and event[0] == SDLK_DOWN
+
+def down_up(event):
+    return event[0] == SDL_KEYUP and event[0] == SDLK_DOWN
+
+def up_down(event):
+    return event[0] == SDL_KEYDOWN and event[0] == SDLK_UP
+
+def up_up(event):
+    return event[0] == SDL_KEYUP and event[0] == SDLK_UP
+
 
 class Spaceship:
     def __init__(self):
         self.x, self.y = 300,300
-        #self.frame = 0
-        #self.face_dir = 1
+        self.frame = 0
+        self.dir_x = 1 #오른쪽
+        self.dir_y = 1 #위쪽
+        self.speed = 5
         self.image = load_image('spaceship_level_1.png')
         self.IDLE, self.MOVE = Idle(self), Move(self)
         self.state_machine = StateMachine(
             self.IDLE,
             {
-                self.MOVE: {right_up: self.IDLE},
-                self.IDLE: {right_down: self.MOVE}
+                self.MOVE: {right_up: self.IDLE, left_up: self.IDLE, up_up: self.IDLE, down_up: self.IDLE},
+                self.IDLE: {right_down: self.MOVE, left_down: self.MOVE, up_down: self.MOVE, down_down: self.MOVE}
             }
         )
 
@@ -34,7 +60,8 @@ class Idle:
         self.spaceship = spaceship
 
     def enter(self):
-        pass
+        self.spaceship.dir_x = 0
+        self.spaceship.dir_y = 0
 
     def exit(self):
         pass
@@ -49,14 +76,33 @@ class Move:
     def __init__(self, spaceship):
         self.spaceship = spaceship
 
-    def enter(self):
-        pass
+    def enter(self, event):
 
-    def exit(self):
+        if right_down(event):
+            self.spaceship.dir_x += 1
+        if left_down(event):
+            self.spaceship.dir_x -= 1
+        if up_down(event):
+            self.spaceship.dir_y += 1
+        if down_down(event):
+            self.spaceship.dir_y -= 1
+
+        if right_up(event):
+            self.spaceship.dir_x -= 1
+        if left_up(event):
+            self.spaceship.dir_x += 1
+        if up_up(event):
+            self.spaceship.dir_y -= 1
+        if down_up(event):
+            self.spaceship.dir_y += 1
+
+
+    def exit(self, event):
         pass
 
     def do(self):
-        pass
+        self.spaceship.x += self.spaceship.dir_x * self.spaceship.speed
+        self.spaceship.y += self.spaceship.dir_y * self.spaceship.speed
 
     def draw(self):
         pass
