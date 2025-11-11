f84f78efbb2c649f62630f4a2228c230e3f06d36
Cho-Jeonghyeon <titanfall120@naver.com>
2025-11-12 04:41:28 +0900

    stage_mode 에서 각 스테이지 버튼 누르면 각 game_mode로 이동

---
+++ stage_button.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+
+class StageButton:
+    def __init__(self, path, x, y, w, h, stage):
+        self.image = load_image(path)
+        self.x, self.y = x, y
+        self.w, self.h = w, h
+        self.stage_mode = stage
+    def draw(self):
+        self.image.draw(self.x, self.y, self.w, self.h)
+
+    def update(self):
+        pass
+
+    def is_clicked(self, mx, my):
+        return (self.x - self.w / 2 <= mx <= self.x + self.w / 2) and (self.y - self.h / 2 <= my <= self.y + self.h / 2)
+
+
--- stage_mode.py
+++ stage_mode.py
@@ -1,33 +1,49 @@
 from pico2d import *
 
 import game_framework
-import game_mode
+import game_mode_1
+import game_mode_2
+import game_mode_3
+import game_world
+from stage_button import StageButton
+from background import Background
 
-stage1 = None
 background = None
 
 def init():
-    global stage1, background
-    stage1 = load_image('plant_1.png')
-    background = load_image('main_background.png')
+    global  background
+
+    background = Background('main_background.png')
+    stage1 = StageButton('plant_1.png', 200, 200, 200, 200, game_mode_1)
+    stage2 = StageButton('plant_2.png', 400, 200, 200, 200, game_mode_2)
+    stage3 = StageButton('plant_3.png', 600, 200, 200, 200, game_mode_3)
+
+    game_world.add_object(background, 0)
+    game_world.add_objects([stage1, stage2, stage3], 1)
+
 def finish():
-    global stage1, background
-    del stage1, background
+    game_world.clear()
 
 def update():
-    pass
+    game_world.update()
 
 def draw():
     clear_canvas()
-    background.draw(600, 500)
-    stage1.draw(200, 200)
+    game_world.render()
     update_canvas()
 
 def handle_events():
     event_list = get_events()
     for event in event_list:
         if event.type == SDL_MOUSEBUTTONDOWN:
-            game_framework.change_mode(game_mode)
+            mx, my = event.x, 1000 - event.y  # 좌표 변환
+
+            print(f"Mouse click: ({mx}, {my})")  #  클릭 좌표 디버깅
+
+            for obj in game_world.world[1]:  # 버튼 레이어
+                if isinstance(obj, StageButton) and obj.is_clicked(mx, my):
+                    print(f" {obj.stage_mode}")  #  연결된 모드 확인
+                    game_framework.change_mode(obj.stage_mode)
 
 def pause():
     pass
